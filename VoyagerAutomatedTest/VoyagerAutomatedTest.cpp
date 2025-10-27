f65f0999e67063c80cba2d2e7eebe9eb5b161526
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-10-26 20:25:49 -0400

    DREAM_72 - created test project & initial Hello Voyager Test project test method (on DREAM-72-Dream-Team-Example.... branch.

---
+++ VoyagerAutomatedTest/VoyagerAutomatedTest.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Voyager/planet.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VoyagerAutomatedTest
+{
+	TEST_CLASS(VoyagerAutomatedTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Logger::WriteMessage("Hello from Dream Team Voyager Test Project\n");
+		}
+	};
+}
+
