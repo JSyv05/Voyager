7327ebafe80139df134f564c5d08cdc30b3373a3
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-10-26 22:13:05 -0400

    Dream-72 - renamed first test case to something meaningful, WIP of 2nd test case to generate planets.

--- VoyagerAutomatedTest/VoyagerAutomatedTest.cpp
+++ VoyagerAutomatedTest/VoyagerAutomatedTest.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Voyager/planet.h"
+#include "../Voyager/planet.cpp"
+#include "../Voyager/game.cpp"
+#include "../Voyager/menu.cpp"
+#include "../Voyager/command.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,9 +13,32 @@ namespace VoyagerAutomatedTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Hello_Voyager)
 		{
 			Logger::WriteMessage("Hello from Dream Team Voyager Test Project\n");
+		}
+
+		TEST_METHOD(Planet_Generation)
+		{
+			Logger::WriteMessage("Testing Planet Generation\n");
+			// generate a number of planets & output using planet class describe command
+			const int NUM_PLANETS = 5;
+			vector <Planet> planets;
+			PlanetGenerator generator;
+
+			std::string outputMessage;
+			std::string outputMessagePointer;
+			outputMessage = "Launching planets " + std::to_string(NUM_PLANETS) + "\n";
+			Logger::WriteMessage(outputMessage.c_str());
+
+			for (int planetNum = 0; planetNum < NUM_PLANETS; planetNum++) {
+				outputMessage = "Planet number " + std::to_string(planetNum) + "\n";
+				Logger::WriteMessage(outputMessage.c_str());
+				planets[planetNum] = std::move(generator.generatePlanet(planetNum));
+				// outputMessage = planets[planetNum].describe();
+				outputMessage = planets[planetNum].getName();
+				Logger::WriteMessage(outputMessage.c_str());
+			}
 		}
 	};
 }
