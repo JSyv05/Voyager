05a28c7ec4e329d645b79602561bed297d0f3a9d
alina <betancesalina23@gmail.com>
2025-11-12 10:31:32 -0500

    DREAM-87 created a plant class

---
+++ Voyager/Plants.h
@@ -0,0 +1,17 @@
+#pragma once
+class Plants
+{
+// constructor Plants(enum plantName, string plantDescription)  // possibly add planet name
+
+		//string displayPlantName()
+
+		//Data structures :
+
+	//enum
+
+		//enum plantNameEnum
+
+		//string plantDescription 
+};
+
+
