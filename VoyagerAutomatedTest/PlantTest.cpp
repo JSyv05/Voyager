6ceff3af54c3293032f4f1973b81d27ec7eb900f
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-11-12 17:11:50 -0500

    dream-87 - adding another test case & swapping the enum from private to public (so the test case & other classes can access it)

--- VoyagerAutomatedTest/PlantTest.cpp
+++ VoyagerAutomatedTest/PlantTest.cpp
@@ -6,6 +6,11 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+/* 
+*  Plants (aka flora) functionality test cases
+*  more details to come once we populate the plants.h header file
+*/
+
 namespace VoyagerAutomatedTest
 {
 	TEST_CLASS(PlantTest)
@@ -16,5 +21,18 @@ namespace VoyagerAutomatedTest
 		{
 			Logger::WriteMessage("Hello from Plant Test Project\n");
 		}
+
+		// create a few plant types & verify created correctly
+		TEST_METHOD(Create_Plants) {
+			Logger::WriteMessage("Creating a few plants & verifying created correctly and accessible");
+			std::string pinkHydrangeaDescription = "a lovely pink hydrangea";
+			std::string blueHydrangeaDescription = "a striking blue hydrangea";
+			Plants pink(Plants::HYDRANGEA, pinkHydrangeaDescription);
+			Plants blue(Plants::HYDRANGEA, blueHydrangeaDescription);
+			Assert::AreEqual(pinkHydrangeaDescription, pink.displayPlantDescription());
+			Assert::AreEqual(blueHydrangeaDescription, blue.displayPlantDescription());
+		}
+
+		// next create some planets - but this needs us to generate the next set of code changes
 	};
 }
