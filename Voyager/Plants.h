9a21d73b2983eb1fa3849539aea5a4b718f9bf8e
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-11-12 22:00:47 -0500

    dream-87 - lost changes to plants.h, remade the changes.

--- Voyager/Plants.h
+++ Voyager/Plants.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 
 class Plants
@@ -7,17 +6,19 @@ class Plants
         // from design subtask
 		// Methods:
 		//   constructor Plants(enum plantName, string plantDescription)
-		//                     ToDo - possibly add planet name in future
-		//   string displayPlantDescription()
+		//    ToDo - possibly add planet name in future
+		//  string displayPlantDescription()
 		//Data structures :
 		//   enum plantNameEnum  (changing to PLANT_TYPES)
 		//   string plantDescription
 public:
 		enum PLANT_TYPES { HYDRANGEA }; // only define a single plant for now, choosing Upper Snake Case as contrast to command enum definition
+
 		Plants(PLANT_TYPES, std::string plantDescription);
 		std::string getPlantDescription();
 
 private:
 	std::string plantDescription_;
 	PLANT_TYPES plantType_;
+
 };
